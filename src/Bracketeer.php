e34e307ec989fc7176d004e248388a7150049b12
Zeolite <tf-91@mail.ru>
2018-01-03 12:39:45 +0300

    First commit

---
+++ src/Bracketeer.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: zeolite
+ * Date: 01.01.18
+ * Time: 20:48
+ */
+class Bracketeer
+{
+    public function isBalanced($s) {
+        $bal = 0;
+        $allowed_chars = array("(", ")", " ", "\n", "\r", "\t");
+        for ($i=0; $i < strlen($s); $i++) {
+            $ch = substr($s, $i, 1);
+            if (in_array($ch, $allowed_chars)) {
+                switch ($ch) {
+                    case '(':
+                        $bal++;
+                        break;
+                    case ')':
+                        $bal--;
+                        break;
+                    default:
+                        continue;
+                }
+                if ($bal < 0) return false;
+            } else {
+                throw new InvalidArgumentException('Input string contains forbidden symbol "' . $ch . '". Allowed chars are: "(", ")", " ", \n", "\t", "\r"');
+            }
+        }
+        return ($bal == 0);
+    }
+}
